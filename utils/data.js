cefbcab119fe9016e2d232e8ba68c242425855ce
rich p <richardferry3@gmail.com>
2022-05-23 04:38:40 -0400

    add function to get random user, thought, and reaction

--- utils/data.js
+++ utils/data.js
@@ -9,7 +9,7 @@ const users = [
     'jenny',
 ];
 
-const thoughts = [
+const thoughtText = [
     'love it',
     'good',
     'wow',
@@ -20,3 +20,42 @@ const thoughts = [
     'fantastic',
 ];
 
+const reactions = [
+    'reaction 1',
+    'reaction 2',
+    'reaction 3',
+    'reaction 4',
+    'reaction 5',
+    'reaction 6',
+    'reaction 7',
+    'reaction 8',
+];
+
+const getRandom = (arr) => arr[Math.floor(Math.random()* arr.length)];
+
+const getRandomUser = () => `${getRandom(users)}`;
+
+const getRandomThought = () => {
+    let results = [];
+    for (i=0;i<3;i++){
+        results.push({
+           thoughtText: getRandom(thoughtText),
+           username: this.username,
+           reactions: getRandomReaction(2),
+        });
+    }
+    return results;
+};
+
+const getRandomReaction = (int) => {
+    let results = [];
+    for (i=0;i<int;i++){
+        results.push({
+            reactionBody: getRandom(reactions),
+            username: getRandom(users),
+        });
+    }
+    return results;
+}
+
+module.exports = { getRandomUser, getRandomThought, getRandom, getRandomReaction,users};
